33f7fb76e0d4fbeffd37abf5fc740718b416fead
Jan Janiszewski <pole19901990@gmail.com>
2022-05-06 20:54:45 +0200

    first commit

--- Pipeline/Z_DataUnpacking.py
+++ Pipeline/Z_DataUnpacking.py
@@ -8,12 +8,13 @@ def unpack_data():
     print('Unpacking data from input folder and saving it in throughput/Z_A folder')
     with zipfile.ZipFile(cf.Z_input, 'r') as zip_ref:
         zip_ref.extractall(cf.Z_Z_throughput)
-    print('Finished unpacking and saving')
+    print('Finished')
 
     print('Renaming and reorganizing text files, deleting audio files')
     folders = os.listdir(cf.Z_Z_zipfolder)
     [os.rename(join(cf.Z_Z_zipfolder, f, 'Text.txt'), join(cf.Z_Z_zipfolder, f'{f}.txt')) for f in folders]
     [shutil.rmtree(join(cf.Z_Z_zipfolder, f)) for f in folders]
+    print('Finished')
 
 if __name__ == '__main__':
     unpack_data()
