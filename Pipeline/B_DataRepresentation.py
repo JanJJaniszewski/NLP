9d61c338e4a3dc66124dbd1f670c73a6ec91fe26
Jan Janiszewski <pole19901990@gmail.com>
2022-05-16 16:01:08 +0200

    added some finbert gitignore stuff

--- Pipeline/B_DataRepresentation.py
+++ Pipeline/B_DataRepresentation.py
@@ -1,1 +1,23 @@
 import config as cf
+import pandas as pd
+import numpy as np
+
+def transform_to_finbert_format():
+    print('Transforming data into Finbert format')
+    texts = pd.read_pickle(cf.texts_and_prices_file)
+    texts['text'] = texts['presentation']
+    texts = texts.dropna()
+    texts['label'] = 'neutral'
+    texts.loc[((texts['price_after'] - texts['price_before']) / texts['price_before']) > 0.01, 'label'] = 'positive'
+    texts.loc[((texts['price_after'] - texts['price_before']) / texts['price_before']) < -0.01, 'label'] = 'negative'
+    for_finbert = texts[['text', 'label']]
+    train, validate, test = \
+        np.split(for_finbert.sample(frac=1, random_state=42),
+                 [int(.6 * len(for_finbert)), int(.8 * len(for_finbert))])
+    train.to_csv(cf.path_train, sep="\t")
+    test.to_csv(cf.path_test, sep="\t")
+    validate.to_csv(cf.path_validate, sep="\t")
+    print(f'Saved all training, test, and validation in Finbert format to {cf.path_train}, {cf.path_validate}, {cf.path_test}')
+
+if __name__ == '__main__':
+    transform_to_finbert_format()
--- config.py
+++ config.py
@@ -4,9 +4,12 @@ from os.path import join
 Z_input = './Data/input/ReleasedDataset.zip'
 Z_A_throughput = './Data/throughput/Z_A'
 A_A_throughput = './Data/throughput/A_A'
-
+A_B_throughput = './Data/throughput/A_B'
 Z_A_zipfolder = './Data/throughput/Z_A/ReleasedDataset_mp3'
 
 
 # File locations
 texts_and_prices_file = join(A_A_throughput, 'df_texts_and_prices.pickle')
+path_train = join(A_B_throughput, 'train.csv')
+path_test = join(A_B_throughput, 'test.csv')
+path_validate = join(A_B_throughput, 'validation.csv')
